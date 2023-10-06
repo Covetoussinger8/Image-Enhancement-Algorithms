4ac2e540321a7af89a5176d5f4e15ff5b60bf083
Leonardo Monteiro de Sousa <leonardo_04monteiro@hotmail.com>
2023-10-06 13:51:22 -0300

    Added histogram_equalization function in enhancement_algorithms.py

--- enhancement_algorithms.py
+++ enhancement_algorithms.py
@@ -2,7 +2,10 @@
     Enhancement algorithms
 """
 
+import time
+import numpy as np
 from PIL import Image
+from skimage import exposure
 
 
 def get_grayscale_image(image_path):
@@ -13,11 +16,32 @@ def get_grayscale_image(image_path):
     """
     try:
         with Image.open(image_path) as image:
-            if image.mode == 'L':
+            if image.mode == 'L':  # Check whether the image is grayscale
                 grayscale_image = image.copy()
-            else:
+            else:  # Else convert it to grayscale
                 grayscale_image = image.convert('L')
     except FileNotFoundError:
         print(f"File not found: {image_path}")
         grayscale_image = None
+
     return grayscale_image
+
+
+def histogram_equalization(image_path):
+    """
+
+    :param image_path:
+    :return:
+    """
+    start_time = time.monotonic_ns()
+
+    image_array = np.asarray(get_grayscale_image(image_path))
+    equalized_image_array = exposure.equalize_hist(image_array)
+    equalized_image = Image.fromarray((equalized_image_array * 255).astype(
+        np.uint8))
+
+    end_time = time.monotonic_ns()
+    runtime_ns = end_time - start_time
+    runtime_milliseconds = runtime_ns / 1e6
+
+    return equalized_image, runtime_milliseconds
